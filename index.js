d1aca49c4800df92c7359714c1e677c446da6f0d
Maxix <account@maxix.sk>
2024-07-04 21:47:56 +0200

    update

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/data', async (req, res) => {
         ORDER BY series.series_id, books.book_id, chapters.chapter_id;
     `;
         const rows = await conn.query(query);
-        
+
         const seriesData = {};
         rows.forEach(row => {
             if (!seriesData[row.series_id]) {
@@ -61,7 +61,7 @@ app.get('/data', async (req, res) => {
                 });
             }
         });
-        
+
         function formatDateToLocal(date) {
             return new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
                 .toISOString()
@@ -84,18 +84,44 @@ app.get('/data', async (req, res) => {
 
 const apiKey = process.env.API_KEY;
 
-app.post('/data', (req, res) => {
-    if (req.headers['api-key'] !== apiKey) {
-        res.status(401).send('Unauthorized');
-        return;
+function checkApiKey(req, res, next) {
+    if (req.headers['api-key'] === apiKey) {
+        next();
+    } else {
+        res.status(401).send('Invalid API key');
     }
-    fs.writeFile('data.json', JSON.stringify(req.body), 'utf8', (err) => {
-        if (err) {
-            res.status(500).send('Error saving data');
-        } else {
-            res.send('Data saved successfully');
+}
+
+app.post('/add-data', checkApiKey, async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const { type, ...data } = req.body;
+
+        switch (type) {
+            case 'series':
+                if (!data.seriesName) return res.status(400).send('Missing series name');
+                await conn.query('INSERT INTO series (seriesName, img) VALUES (?, ?)', [data.seriesName, data.img]);
+                break;
+            case 'book':
+                if (!data.name || !data.series_id) return res.status(400).send('Missing book name or series ID');
+                await conn.query('INSERT INTO books (name, startedReading, endedReading, series_id) VALUES (?, ?, ?, ?)', [data.name, data.startedReading, data.endedReading, data.series_id]);
+                break;
+            case 'chapter':
+                if (!data.name || !data.book_id) return res.status(400).send('Missing chapter name or book ID');
+                await conn.query('INSERT INTO chapters (name, date, book_id) VALUES (?, ?, ?)', [data.name, data.date, data.book_id]);
+                break;
+            default:
+                return res.status(400).send('Invalid type specified');
         }
-    });
+
+        res.send(`${type} added successfully`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error adding data');
+    } finally {
+        if (conn) conn.end();
+    }
 });
 
 app.listen(7000, () => {
