8ddeb36a23ddf6bb1eff1d8285f3d3eb5d5817ff
Maxix <account@maxix.sk>
2024-07-05 18:32:36 +0200

    update

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
+pool.getConnection()
+    .then(conn => {
+        console.log('Server-chan: Connected to the DB!');
+        conn.release(); 
+    })
+    .catch(err => {
+        console.error('Server-chan: Error connecting to the DB:', err);
+    });
+
 app.get('/', (req, res) => {
     res.send('mDatabase-chan: Ahoooj!');
 });
@@ -92,6 +101,10 @@ function checkApiKey(req, res, next) {
     }
 }
 
+app.post('/admin', checkApiKey , async (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.post('/add-data', checkApiKey, async (req, res) => {
     let conn;
     try {
@@ -124,6 +137,106 @@ app.post('/add-data', checkApiKey, async (req, res) => {
     }
 });
 
+app.put('/update-data/:type/:id', checkApiKey, async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const { type, id } = req.params;
+        const { name, startedReading, endedReading, date } = req.body;
+
+        switch (type) {
+            case 'series':
+                await conn.query('UPDATE series SET seriesName = ? WHERE series_id = ?', [name, id]);
+                break;
+            case 'book':
+                await conn.query('UPDATE books SET name = ?, startedReading = ?, endedReading = ? WHERE book_id = ?', [name, startedReading, endedReading, id]);
+                break;
+            case 'chapter':
+                await conn.query('UPDATE chapters SET name = ?, date = ? WHERE chapter_id = ?', [name, date, id]);
+                break;
+            default:
+                return res.status(400).send('Invalid type specified');
+        }
+
+        res.send(`${type} updated successfully`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error updating data');
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+
+app.delete('/delete-data/:type/:id', checkApiKey, async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const { type, id } = req.params;
+
+        switch (type) {
+            case 'series':
+                await conn.query('DELETE FROM series WHERE series_id = ?', [id]);
+                break;
+            case 'book':
+                await conn.query('DELETE FROM books WHERE book_id = ?', [id]);
+                break;
+            case 'chapter':
+                await conn.query('DELETE FROM chapters WHERE chapter_id = ?', [id]);
+                break;
+            default:
+                return res.status(400).send('Invalid type specified');
+        }
+
+        res.send(`${type} deleted successfully`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting data');
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+app.get('/list-series', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const query = 'SELECT series_id, seriesName FROM series';
+        const rows = await conn.query(query);
+        res.send(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error reading data');
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+app.get('/list-books/:series_id', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const { series_id } = req.params;
+        const query = 'SELECT book_id, name FROM books WHERE series_id = ?';
+        const rows = await conn.query(query, [series_id]);
+        res.send(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error reading data');
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+app.use((req, res, next) => {
+    res.status(404).send({ msg: '404-chan: Nenašla som to!' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ msg: '500-chan: Niečo sa pokazilo!' });
+});
+
 app.listen(7000, () => {
-    console.log('mDatabase-chan: Bežím na porte 7000');
+    console.log('Server-chan: Listening on port 7000');
 });
