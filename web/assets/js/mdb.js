8f622d12463d0091c008898839723af634e3a878
MaxixSVK <dev@maxix.sk>
2024-11-19 20:41:44 +0100

    Fix lowres image URL handling in renderSeriesCard and renderBookCard functions

--- web/assets/js/mdb.js
+++ web/assets/js/mdb.js
@@ -59,7 +59,7 @@ function renderSeriesCard(series) {
     header.className = 'flex items-center';
 
     const img = document.createElement('img');
-    img.src = series.img + '?lowres=true' || 'https://apimdb.maxix.sk/cdn/images/404.png';
+    img.src = series.img ? series.img + '?lowres=true' : 'https://apimdb.maxix.sk/cdn/images/404.png?lowres=true';
     img.alt = series.name || 'No image';
     img.className = 'h-24 object-cover rounded-md mr-4';
 
@@ -136,7 +136,7 @@ function renderBookCard(book) {
     card.className = 'bg-[#2A2A2A] rounded-md mt-4 flex items-center transition transform duration-500 ease-in-out opacity-0 translate-y-4';
 
     const img = document.createElement('img');
-    img.src = book.img + '?lowres=true'  || 'https://apimdb.maxix.sk/cdn/images/404.png';
+    img.src = book.img ? book.img + '?lowres=true' : 'https://apimdb.maxix.sk/cdn/images/404.png?lowres=true';
     img.alt = book.name || 'No image';
     img.className = 'h-24 object-cover rounded-md mr-4';
 
