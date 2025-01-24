cef3a0ba9a9d2048a083f0c1434201dc57eceb0e
MaxixSVK <dev@maxix.sk>
2025-01-24 23:05:12 +0100

    moving backup from dashboard to acp and formatting

--- web/assets/js/acp.js
+++ web/assets/js/acp.js
@@ -8,7 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
         lineWrapping: true
     });
 
-    document.getElementById('restart-server').addEventListener('click', restartServer);
+    setupButtonEvent('db-backup', 'click', dbBackup);
+    setupButtonEvent('cdn-backup', 'click', cdnBackup);
+    setupButtonEvent('restart-server', 'click', restartServer);
 
     document.getElementById('config-form').addEventListener('submit', function (event) {
         event.preventDefault();
@@ -17,6 +19,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     getCurrentConfig(configEditor);
 });
+
+function setupButtonEvent(elementId, eventType, handler) {
+    const element = document.getElementById(elementId);
+    element.addEventListener(eventType, handler);
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
 async function checkLogin() {
     const session = getCookie('sessionToken');
@@ -30,7 +48,9 @@ async function checkLogin() {
                 },
             });
 
-            if (!response.ok) {
+            if (response.ok) {
+                displayUser();
+            } else {
                 document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
                 window.location.href = '/auth';
             }
@@ -42,15 +62,98 @@ async function checkLogin() {
     }
 }
 
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+async function displayUser() {
+    try {
+        const response = await fetch(api + '/account', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': getCookie('sessionToken')
+            },
+        });
+        const data = await response.json();
+        document.getElementById('username').textContent = data.username
+        document.getElementById('useremail').textContent = data.email
+        document.getElementById('login').classList.add('hidden');
+        document.getElementById('pfp').classList.remove('hidden');
+        document.getElementById('pfp').src = cdn + '/users/pfp/' + data.username + '.png';
+    } catch (error) {
+        console.error('Error fetching user:', error);
     }
-    return null;
+}
+
+function dbBackup() {
+    try {
+        showNotification('Sending request to backup database, please wait this may take a while', 'info');
+        fetch(api + '/backup-db', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getCookie('sessionToken')
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = `mdb-db-backup-${new Date().toISOString().split('T')[0]}.sql`;
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                showNotification('Database backup successful', 'success');
+            })
+            .catch(error => {
+                console.error('Backup failed:', error);
+                showNotification('Database backup failed', 'error');
+            });
+    } catch (error) {
+        console.error('Backup failed:', error);
+        showNotification('Database backup failed', 'error');
+    }
+}
+
+async function cdnBackup() {
+    try {
+        showNotification('Sending request to backup CDN, please wait this may take a while', 'info');
+        fetch(cdn + '/backup', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getCookie('sessionToken')
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = `mdb-cdn-backup-${new Date().toISOString().split('T')[0]}.zip`;
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                showNotification('CDN backup successful', 'success');
+            })
+            .catch(error => {
+                console.error('Backup failed:', error);
+                showNotification('CDN backup failed', 'error');
+            });
+    } catch (error) {
+        console.error('Backup failed:', error);
+        showNotification('CDN backup failed', 'error');
+    }
 }
 
 function restartServer() {
--- web/assets/js/mdbAdmin.js
+++ web/assets/js/mdbAdmin.js
@@ -5,8 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function setupEventListeners() {
     setupButtonEvent('logout', 'click', handleLogout);
-    setupButtonEvent('db-backup', 'click', dbBackup);
-    setupButtonEvent('cdn-backup', 'click', cdnBackup);
     setupToggleEvent('passwordHeader', 'changePassowrd');
     setupToggleEvent('sessionHeader', 'sessionList');
     setupFormSubmitEvent('cdn-upload-form', handleCDNUpload);
@@ -320,80 +318,6 @@ async function destroySession(sessionId) {
         }
     } catch (error) {
         console.error('Error destroying session:', error);
-    }
-}
-
-function dbBackup() {
-    try {
-        showNotification('Sending request to backup database, please wait this may take a while', 'info');
-        fetch(api + '/backup-db', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getCookie('sessionToken')
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.blob();
-            })
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                a.download = `mdb-db-backup-${new Date().toISOString().split('T')[0]}.sql`;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-                showNotification('Database backup successful', 'success');
-            })
-            .catch(error => {
-                console.error('Backup failed:', error);
-                showNotification('Database backup failed', 'error');
-            });
-    } catch (error) {
-        console.error('Backup failed:', error);
-        showNotification('Database backup failed', 'error');
-    }
-}
-
-async function cdnBackup() {
-    try {
-        showNotification('Sending request to backup CDN, please wait this may take a while', 'info');
-        fetch(cdn + '/backup', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getCookie('sessionToken')
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.blob();
-            })
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                a.download = `mdb-cdn-backup-${new Date().toISOString().split('T')[0]}.zip`;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-                showNotification('CDN backup successful', 'success');
-            })
-            .catch(error => {
-                console.error('Backup failed:', error);
-                showNotification('CDN backup failed', 'error');
-            });
-    } catch (error) {
-        console.error('Backup failed:', error);
-        showNotification('CDN backup failed', 'error');
     }
 }
 
